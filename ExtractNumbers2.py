1c2e264de3569a7cae3086dcc29175faba019c41
GoopSoftware <GoopSoftware@users.noreply.github.com>
2024-03-26 16:30:15 -0600

    Split the picture in 3 parts for better number recognition

--- ExtractNumbers2.py
+++ ExtractNumbers2.py
@@ -6,6 +6,16 @@ import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+
+def split_image(image):
+    height, width = image.shape
+    section_height = height // 3
+    top_section = image[0:section_height, :]
+    middle_section = image[section_height:2*section_height, :]
+    bottom_section = image[2*section_height:3*section_height, :]
+    return top_section, middle_section, bottom_section
+
+
 # Get image
 image_path = 'downloaded_images/FFVIII_Caterchipillar_monster_card.png'
 folder_path = 'downloaded_images'
@@ -19,37 +29,45 @@ for image_name in os.listdir(folder_path):
     original_image = cv2.imread(files)
     cropped_image = original_image[4:52, 5:33]
     gray_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-    (thresh, blackAndWhiteImage) = cv2.threshold(gray_image, 220, 255, cv2.THRESH_BINARY)
+    (thresh, blackAndWhiteImage) = cv2.threshold(gray_image, 240, 255, cv2.THRESH_BINARY)
     inverted_image = 255 - blackAndWhiteImage
 
-    # Take the previous code and apply a variable for later code
-    image_to_process = inverted_image
+    top_section, middle_section, bottom_section = split_image(inverted_image)
 
-    # OCR the text
-    text = pytesseract.image_to_string(image_to_process, config='--psm 10 --oem 3 -c tessedit_char_whitelist=A0123456789')
+    sections = [top_section, middle_section, bottom_section]
+    extracted_numbers = []
+
+    for i, section in enumerate(sections, start=1):
+        # OCR the text
+        text = pytesseract.image_to_string(section,
+                                           config='--psm 10 --oem 3 -c '
+                                                  'tessedit_char_whitelist=A0123456789')
+        numbers = ''.join(filter(lambda char: char.isdigit() or char == 'A', text))
+        extracted_numbers.extend(numbers)
+
+        # Save the image
+        output_folder = 'bw_images'
+        output_name = os.path.splitext(image_name)[0] + f'_{i}_bw.png'
+        output_path = os.path.join(output_folder, output_name)
+        cv2.imwrite(output_path, section)
+
     counter += 1
-    numbers = text
     print(f'{counter} Image: {image_name}'.strip('\n'))
-    print("Extracted numbers:", numbers.strip('\n'))
+    print("Extracted numbers:", ''.join(extracted_numbers))
 
     # Calculate accuracy
-    extracted_numbers = [int(num) for num in numbers if num.isdigit()]
     accuracy = len(extracted_numbers)
     total_accuracy += accuracy
 
-    # Save the image
-    output_folder = 'bw_images'
-    output_name = os.path.splitext(image_name)[0] + '_bw.png'
-    output_path = os.path.join(output_folder, output_name)
-    cv2.imwrite(output_path, image_to_process)
+
 
 average_accuracy = total_accuracy / 440
-average_accuracy = float(round(average_accuracy, 2))
+average_accuracy = round(average_accuracy, 2)
 print(f"Accuracy: {average_accuracy * 100}%")
 
 # Show the image
-plt.imshow(image_to_process, cmap='gray')
-plt.axis('off')
-plt.show()
+#plt.imshow(image_to_process, cmap='gray')
+#plt.axis('off')
+#plt.show()
 
 
